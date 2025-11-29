17c44523fcf37ad3215523b2989f45a0742a8f6c
xdilobar2197 <xdilobar2197@gmail.com>
2025-11-29 17:05:46 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# mashiqlar2_2911
+# 6 - masala
+string_uzunlg = lambda n:len(n)
+natija = string_uzunlg("Salom")
+print(natija)
+
+
+
+# 7 - masala
+tekshir = lambda x: "musbat" if x > 0 else ("manfiy" if x < 0 else "nol")
+print(tekshir(4))
+
+
+
+# 8 - masala
+uchburjak = lambda asos, balandlik: 0.5 * asos * balandlik
+natija = uchburjak(22, 11)
+print(natija)
+
+# 9 - masala
+toq_juft = lambda x: x % 2 == 0
+natija = toq_juft(66)
+print(natija)
+
+
+# 10 - masala
+son = lambda x: x > 10
+natija = son(77)
+print(natija)
